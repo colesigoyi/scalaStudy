08219f87c06a80912985f476de3ef5136293f188
taoxuefeng <tao642133424@163.com>
2019-10-31 16:04:44 +0800

    generic

--- scala_studying/src/main/scala/cn/qf/day09/demo03_transformation/RDDTransformationDemo.scala
+++ scala_studying/src/main/scala/cn/qf/day09/demo03_transformation/RDDTransformationDemo.scala
@@ -35,22 +35,62 @@ object RDDTransformationDemo {
     //map:参数指定的函数依次作用域rdd中每个元素,将函数执行后的结果置于全新的rdd中存储起来
     val resultRDD = rdd.map(_ * 2)
     println(s"每个元素乘以2后新的RDD中的元素信息是:${resultRDD.collect().toBuffer}")
-    println("------------------------------")
 
+    println("---------------filter:将旧的rdd内容中的元素根据指定的规则过滤筛选后置于新的rdd中---------------")
     //filter:将旧的rdd内容中的元素根据指定的规则过滤筛选后置于新的rdd中
     println(s"筛选奇数后新的RDD中的元素信息是:${rdd.filter(_ % 2 != 0).collect().toBuffer}")
-    println("------------------------------")
 
+    println("--------------flatMap:map + flattern:rdd中待分析的元素是集合类型----------------")
     //flatMap:map + flattern:rdd中待分析的元素是集合类型
     val rddTmp:RDD[String] = sc.parallelize(Seq("张无忌 18", "张三丰 89"))
     rddTmp.flatMap(_.split("\\s+")).foreach(println)
-    println("------------------------------")
+
+    println("-------------sample随机采样-----------------")
     //sample随机采样
     sc.parallelize(1 to 10)
+      //sample算子有如下参数:
+      //withReplacement
+      //Fraction:
+      //seed:
       .sample(withReplacement = false, 0.5)//false表示不放回,新的rdd中有新的元素就不放回
       .collect()
       .toBuffer
         .foreach(println)
+
+    println("--------------union:将两个rdd中的所有元素合并----------------")
+    //union:将两个rdd中的所有元素合并
+    sc.parallelize(1 to 5).union(sc.parallelize(5 to 10)).collect().foreach(println)
+
+    println("--------------intersection:求两个RDD交集----------------")
+    //intersection:求两个RDD交集
+    sc.parallelize(1 to 7).intersection(sc.parallelize(3 to 10)).collect().foreach(println)
+
+    println("--------------distinct:将rdd中的元素去重----------------")
+    //distinct:将rdd中的元素去重
+    sc.parallelize(1 to 7).union(sc.parallelize(5 to 10)).distinct().collect().foreach(println)
+
+    println("--------------join:两个rdd进行内连接操作----------------")
+    //join:两个rdd进行内连接操作
+    val s1 = sc.parallelize(Seq(("tom", 19),("marry", 20),("jack",23)))
+    val s2 = sc.parallelize(Seq(("tom", "杭州"),("marry", "武汉"),("lucy","合肥")))
+    val joinRDD:RDD[(String,(Int, String))] = s1.join(s2)
+    println("名字  年龄  籍贯")
+    joinRDD.foreach(perEle => println(perEle._1 + " " + perEle._2._1 + " " + perEle._2._2))
+
+    println("----------------leftOuterJoin:左外连接--------------")
+    //leftOuterJoin:左外连接
+    val result:RDD[(String,(Int, Option[String]))] = s1.leftOuterJoin(s2)
+    result.foreach(println)
+
+    println("----------------rightOuterJoin:右外连接--------------")
+    //rightOuterJoin:右外连接
+    val result2:RDD[(String,(Option[Int], String))] = s1.rightOuterJoin(s2)
+    result2.foreach(println)
+
+    println("----------------cartesian:求笛卡尔积--------------")
+    //cartesian:求笛卡尔积
+    val result3:RDD[((String,Int),(String,String))] = s1.cartesian(s2)
+    result3.foreach(println)
     //资源释放
     spark.stop()
   }
